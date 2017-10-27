22a8e03545fea2ea675f3f0629372d1fe5bc28ea
aaronhughey <30471014+aaronhughey@users.noreply.github.com>
2017-10-27 09:30:49 -0400

    update

--- BitsandPizzas/app/src/main/java/com/hfad/bitsandpizzas/MainActivity.java
+++ BitsandPizzas/app/src/main/java/com/hfad/bitsandpizzas/MainActivity.java
@@ -86,6 +86,8 @@ public class MainActivity extends AppCompatActivity {
                     return new PastaFragment();
                 case 3:
                     return new StoresFragment();
+                case 4:
+                    return new SaladFragment();
             }
             return null;
         }
@@ -101,6 +103,8 @@ public class MainActivity extends AppCompatActivity {
                     return getResources().getText(R.string.pasta_tab);
                 case 3:
                     return getResources().getText(R.string.store_tab);
+                case 4:
+                    return "Salads";
             }
             return null;
         }
